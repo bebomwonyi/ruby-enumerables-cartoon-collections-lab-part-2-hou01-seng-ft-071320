c0b7f9f7c0391d56cd7f3505f5689d2ef86c8054
Benjamin Ebomwonyi <bebomwonyi@gmail.com>
2020-07-08 13:17:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  s = planeteer_calls.find.include {|a| a==valid_calls}
+  s = planeteer_calls.find {|a| valid_calls.includes}
   return s
 end
